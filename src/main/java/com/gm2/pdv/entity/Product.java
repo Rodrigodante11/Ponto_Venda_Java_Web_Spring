e4d3e457896100c3c9e2daab590f0424bee74fb8
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-01-10 13:14:58 -0300

    implementando as entidades usuario e produto

---
+++ src/main/java/com/gm2/pdv/entity/Product.java
@@ -0,0 +1,33 @@
+package com.gm2.pdv.entity;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "product")
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String description;
+
+    @Column(length = 20, precision = 20, scale = 2,nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private int quantity;
+
+
+
+}
--- src/main/java/com/gm2/pdv/entity/User.java
+++ src/main/java/com/gm2/pdv/entity/User.java
@@ -1,18 +1,30 @@
 package com.gm2.pdv.entity;
 
-import lombok.*;
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
 
 
-@Getter
-@Setter
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@ToString
-@EqualsAndHashCode
+@Entity
+@Table(name = "user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 100, nullable = false)
     private String name;
     private boolean isEnable;
 
