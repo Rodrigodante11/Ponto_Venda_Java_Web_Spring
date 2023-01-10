217132409121c2604f84a1568d238e79ea1e2fa0
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-01-10 11:42:28 -0300

    first commit

---
+++ src/main/java/com/gm2/pdv/pdv/PdvApplication.java
@@ -0,0 +1,13 @@
+package com.gm2.pdv.pdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gm2/pdv/pdv/PdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm2.pdv.pdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
